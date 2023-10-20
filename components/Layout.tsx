06225c42cdea6e0d7bbe267e0f0122d329f4afca
Kade <kadekorn@gmail.com>
2023-10-20 07:17:50 -0700

    I am learning alot currently, quarter of the way through the layout section

---
+++ components/Layout.tsx
@@ -0,0 +1,28 @@
+import Sidebar from "./Layout/Sidebar";
+
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+        <div className="grid grid-cols-4 h-full">
+          <Sidebar />
+          <div
+            className="col-span-
+                 lg:col-span-2
+                 border-x-[1px]
+                 border-neautral-800"
+          >
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ components/Layout/Sidebar.tsx
@@ -0,0 +1,16 @@
+import {BsHouseFill} from 'react-icons/bs'
+
+const Sidebar = () => {
+    const items = [
+        {
+        label: 'Home',
+        href: '/',
+        icon: BsHouseFill,
+        }
+    ]
+  return (
+    <div></div>
+  )
+}
+
+export default Sidebar
