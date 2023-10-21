b49a8df6845de5d69118336ce210308e687bf639
Kade <kadekorn@gmail.com>
2023-10-21 11:57:07 -0700

    added a header feature with a back arrow as well

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
   <div className='text-3xl text-sky-500'>
-  Hello World
+  <>
+  <Header showBackArrow label="Home" />
+  </>
   </div>    
   )
 }
