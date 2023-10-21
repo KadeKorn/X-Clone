14744ee23baa7a31ee533a08213163f8799be247
Kade <kadekorn@gmail.com>
2023-10-21 07:55:54 -0700

    Added a m obile and desktop view, with mobile first, added a dynamic label for d esktop view

--- components/Layout/SidebarItem.tsx
+++ components/Layout/SidebarItem.tsx
@@ -12,12 +12,18 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
     icon: Icon,
     onClick,  // Fixed the typo here
   }) => {
-    return (
+    {/* Mobile first */} return ( 
       <div className="flex flex-row items-center" onClick={onClick}>
         <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
           <Icon size={28} color="white"/>
         </div>
-        {/* You can use label and href here if needed */}
+        {/* Desktop view */}
+        <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-slate-300 hover:bg-opacity-10 ">
+            <Icon size={24} color="white"/>
+            <p>
+                {label}   
+            </p>        
+        </div>
       </div>
     );
   };
