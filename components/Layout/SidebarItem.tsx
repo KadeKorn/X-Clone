4d312ebec9e75fc5fe0d6371d9c8f3d75e62c620
Kade <kadekorn@gmail.com>
2023-10-21 11:20:23 -0700

    Added mobile and desktop versions of the app for the sidebar. Icons, labels, and tweet button

--- components/Layout/SidebarItem.tsx
+++ components/Layout/SidebarItem.tsx
@@ -1,31 +1,31 @@
-
 interface SidebarItemProps {
-    label: string;
-    href?: string;
-    icon: Icon,
-    onClick?: () => void;  // Updated the type here
-  }
+  label: string;
+  href?: string;
+  icon: Icon;
+  onClick?: () => void; // Updated the type here
+}
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
-    label,
-    href,
-    icon: Icon,
-    onClick,  // Fixed the typo here
-  }) => {
-    {/* Mobile first */} return ( 
-      <div className="flex flex-row items-center" onClick={onClick}>
-        <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
-          <Icon size={28} color="white"/>
-        </div>
-        {/* Desktop view */}
-        <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-slate-300 hover:bg-opacity-10 ">
-            <Icon size={24} color="white"/>
-            <p>
-                {label}   
-            </p>        
-        </div>
+  label,
+  href,
+  icon: Icon,
+  onClick, // Fixed the typo here
+}) => {
+  {
+    /* Mobile first */
+  }
+  return (
+    <div className="flex flex-row items-center" onClick={onClick}>
+      <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
+        <Icon size={28} color="white" />
       </div>
-    );
-  };
-  
-  export default SidebarItem;
+      {/* Desktop view */}
+      <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-slate-300 hover:bg-opacity-10 ">
+        <Icon size={24} color="white" />
+        <p className="hidden lg:block text-white text-xl">{label}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarItem;
