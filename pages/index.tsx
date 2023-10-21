414fd25adbe42c3885a6635e2c942bd44876a767
Kade <kadekorn@gmail.com>
2023-10-21 12:13:54 -0700

    finished up with my button.tsx and now I am going to be focused on Auth UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
   <div className='text-3xl text-sky-500'>
   <>
-  <Header showBackArrow label="Home" />
+  <Header label="Home" />
   </>
   </div>    
   )
