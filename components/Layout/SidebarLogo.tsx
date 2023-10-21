9271f7a03618eb7fbcfa56c3ae649a10bf571c32
Kade <kadekorn@gmail.com>
2023-10-21 07:27:32 -0700

    Learning how type script works. I had prior knowledge that you declare your datatypes, but this is working nicely. Using the interface and for the props listing out exactly what it is. Very cool

--- components/Layout/SidebarLogo.tsx
+++ components/Layout/SidebarLogo.tsx
@@ -1,7 +1,29 @@
-const SidebarLogo = () => {
-  return (
-    <div></div>
-  )
-}
+import { useRouter } from "next/router";
+import { FaTwitter } from "react-icons/fa"; // Ill change this later
 
-export default SidebarLogo
+const SidebarLogo = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <div
+      onClick={() => router.push('/')}
+        className="
+    rounded-full
+    h-14 
+    w-14
+    p-4
+    flex
+    items-center
+    justify-center
+    hover:bg-blue-300
+    hover:bg-opacity-10
+    cursor-pointer
+    transition"
+      >
+        <FaTwitter size={28} color="white" />
+      </div>
+    </div>
+  );
+};
+
+export default SidebarLogo;
