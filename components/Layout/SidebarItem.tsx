412776b97bac6f654b6d3b7e9e154970e6cb2c7c
Kade <kadekorn@gmail.com>
2023-10-21 07:41:33 -0700

    I now have the icons working on the side, interesting.

--- components/Layout/SidebarItem.tsx
+++ components/Layout/SidebarItem.tsx
@@ -1,25 +1,25 @@
-import { IconType } from "react-icons";
 
 interface SidebarItemProps {
-  label: string;
-  href?: string;
-  icon: IconType;
-  omClick?: () => JSX.Element;
-}
+    label: string;
+    href?: string;
+    icon: Icon,
+    onClick?: () => void;  // Updated the type here
+  }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
-  label,
-  href,
-  icon: Icon,
-  onClick,
-}) => {
-  return (
-    <div className="flex flex-row items-center">
-      <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
-
+    label,
+    href,
+    icon: Icon,
+    onClick,  // Fixed the typo here
+  }) => {
+    return (
+      <div className="flex flex-row items-center" onClick={onClick}>
+        <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
+          <Icon size={28} color="white"/>
+        </div>
+        {/* You can use label and href here if needed */}
       </div>
-    </div>
-  );
-};
-
-export default SidebarItem;
+    );
+  };
+  
+  export default SidebarItem;
