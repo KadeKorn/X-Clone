892702a572e00ea8b02c2cdf4ded52a47c6918b9
Kade <kadekorn@gmail.com>
2023-10-22 09:13:21 -0700

    added a lot to the loginmodel, which is setting up our auth ui

--- hooks/useLoginModal.ts
+++ hooks/useLoginModal.ts
@@ -8,8 +8,8 @@ interface LoginModalStore {
 
 const useLoginModal = create<LoginModalStore>((set) => ({
     isOpen: false,
-    onOpen () => set({ isOpen: true }),
-    onClose: () => set({ isopen:false }),
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen:false }),
 }))
 
 export default useLoginModal;
