0a6499207ff9a1719367e9e8f0f5bd2fdf6c4be8
Kade <kadekorn@gmail.com>
2023-10-23 11:55:34 -0700

    gave functionality to both loginmodal and register modal in the footer, so if you click sign in it brings you to the login and if you click create an account it bring you to the register

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,14 +1,25 @@
 import useLoginModal from "@/hooks/useLoginModal";
 import { useCallback, useState } from "react";
+import useRegisterModal from "@/hooks/useRegisterModal";
+
 import Input from "../input";
 import Modal from "../Modal";
 
 const LoginModal = () => {
   const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
+  const onToggle = useCallback(() => {
+    if (isLoading) {
+      return;
+    }
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [isLoading, registerModal, loginModal]);
 
   const onSubmit = useCallback(async () => {
     try {
@@ -39,6 +50,19 @@ const LoginModal = () => {
       />
     </div>
   );
+  const footerContent = (
+    <div className="text-neutral-400 text-center mt-4">
+      <p>
+        First time using X?{" "}
+        <span
+          onClick={onToggle}
+          className="text-white cursor-pointer hover:underline"
+        >
+          Create an account
+        </span>
+      </p>
+    </div>
+  );
   return (
     <Modal
       disabled={isLoading}
@@ -48,6 +72,7 @@ const LoginModal = () => {
       onClose={loginModal.onClose}
       onSubmit={onSubmit}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
