afe6e0a34b9bdd35d810e691f7ff9f9d7031ed42
Kade <kadekorn@gmail.com>
2023-11-01 16:42:26 -0700

    just finished up my register and added some good QOL improvements to it.

---
+++ pages/api/register.tsx
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcrypt";
+import prisma from "@/libs/prismadb";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  try {
+    const { email, username, name, password } = req.body;
+
+    // Ensure all required fields are provided
+    if (!email || !username || !name || !password) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        username,
+        name,
+        hashedPassword  
+      },
+    });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "An error occurred while registering the user." });
+  }
+}
