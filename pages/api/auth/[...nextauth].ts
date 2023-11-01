e294faa33f88a12a8d81f50d5281c0369dce9dbb
Kade <kadekorn@gmail.com>
2023-11-01 16:25:30 -0700

    I have finished the next auth and added to my env file

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import NextAuth from "next-auth";
-import { CredentialsProvider } from "next-auth/providers/credentials";
+import Credentials from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import prisma from "@/libs/prismadb";
@@ -10,7 +10,7 @@ import prisma from "@/libs/prismadb";
 export default NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
-        CredentialsProvider({
+        Credentials({
             name: 'credentials',
             credentials: {
                 email: { label: 'email', type: 'text'},
