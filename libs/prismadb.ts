bb516266d34e7fc071eedf04c799a79080e4b9a8
Kade <kadekorn@gmail.com>
2023-11-04 07:22:32 -0700

    added notes for prismadb.ts

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -8,3 +8,8 @@ const client = globalThis.prisma || new PrismaClient()
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
 
 export default client;
+
+//The code initializes and configures a PrismaClient instance for interacting with the database.
+//It checks if a global instance of PrismaClient is already present to enable reusing the same instance across the application (particularly in a non-production environment).
+//It ensures that in a development environment, the application does not create a new instance of PrismaClient on each import, which conserves resources and maintains performance.
+//The configured PrismaClient is made available as a default export for use in other parts of the application.
