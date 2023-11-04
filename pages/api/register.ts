f9334569b9bc8f0824da1ee97fd5a782b8cc297c
Kade <kadekorn@gmail.com>
2023-11-04 07:41:39 -0700

    found a couple of typos,but we are making out way through this

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt";
 
-import prisma from "@/libs/prismadb";
+import prisma from "@/libs/prisma db";
 
 export default async function handler(
   req: NextApiRequest,
@@ -14,10 +14,7 @@ export default async function handler(
   try {
     const { email, username, name, password } = req.body;
 
-    // Ensure all required fields are provided
-    if (!email || !username || !name || !password) {
-      return res.status(400).json({ error: "All fields are required" });
-    }
+  
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
@@ -33,6 +30,46 @@ export default async function handler(
     return res.status(200).json(user);
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ error: "An error occurred while registering the user." });
+    return res.status(400).end();
   }
 }
+
+
+// import { NextApiRequest, NextApiResponse } from "next";
+// import bcrypt from "bcrypt";
+
+// import prisma from "@/libs/prismadb";
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method !== "POST") {
+//     return res.status(405).end();
+//   }
+
+//   try {
+//     const { email, username, name, password } = req.body;
+
+//     // Ensure all required fields are provided
+//     if (!email || !username || !name || !password) {
+//       return res.status(400).json({ error: "All fields are required" });
+//     }
+
+//     const hashedPassword = await bcrypt.hash(password, 12);
+
+//     const user = await prisma.user.create({
+//       data: {
+//         email,
+//         username,
+//         name,
+//         hashedPassword  // Ensure this field is correctly named in your Prisma model
+//       },
+//     });
+
+//     return res.status(200).json(user);
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).json({ error: "An error occurred while registering the user." });
+//   }
+// }
