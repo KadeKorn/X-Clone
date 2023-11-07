e1bb23009217573057a5622e76dc232cc10038ef
Kade <kadekorn@gmail.com>
2023-11-07 06:12:27 -0700

    for whatever reason my userBio wasn't working, so I deleted it from my userId and added it back and now its working

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -19,18 +19,18 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
     if (!fetchedUser?.createdAt) {
       return null;
     }
-    return format(new Date(fetchedUser.createdAt), `MMMM yyyy`);
+    return format(new Date(fetchedUser.createdAt), 'MMMM yyyy');
   }, [fetchedUser?.createdAt]);
 
   return (
-    <div className="border-b-[1px] border-neutral-800 pb-4">
-      <div className="flex justify-end p-2">
+    <div className="border-b-[8px] border-red-800 pb-4">
+      {/* <div className="flex justify-end p-2">
         {currentUser?.id == userId ? (
           <Button secondary label="Edit" onClick={() => {}} />
         ) : (
           <Button onClick={() => {}} label="Follow" secondary />
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
