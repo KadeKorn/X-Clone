40a3156928f2a66e9bb8be5e37138c3d63180514
Kade <kadekorn@gmail.com>
2023-11-07 06:31:55 -0700

    I changed the formatting so my child classes/elements are able to take the parent classes. Great great job

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -6,6 +6,7 @@ import useUser from "@/hooks/useUser";
 import Button from "../Button";
 
 import { useMemo } from "react";
+import { BiCalendar } from "react-icons/bi";
 
 interface UserBioProps {
   userId: string;
@@ -36,6 +37,14 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
           <p className="text-white text-2xl font-semibold">
             {fetchedUser?.name}
           </p>
+          <p className="text-md text-neutral-500">@{fetchedUser?.username}</p>
+        </div>
+        <div className="flex flex-col mt-4">
+          <p className="text-white">{fetchedUser?.bio}</p>
+          <div className="flex flex-row items-center gap-2 mt-4 text-neutral-500">
+            <BiCalendar size={24}  />
+            <p>Joined {createdAt}</p>
+          </div>
         </div>
       </div>
     </div>
