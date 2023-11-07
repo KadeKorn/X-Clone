fa25b44cdcdfe1cb49e20f47016ec2c9a1022100
Kade <kadekorn@gmail.com>
2023-11-07 06:38:21 -0700

    Finished up the user profile part of this and its amazing. I am really enjoying doing parrall coding

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -46,6 +46,24 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
             <p>Joined {createdAt}</p>
           </div>
         </div>
+        <div className="flex flex-row items-center mt-4 gap-5">
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">
+              {fetchedUser?.followingIds?.length}
+            </p>
+            <p className="text-neutral-500">
+              Following
+            </p>
+          </div>
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">
+              {fetchedUser?.followersCount || 0}
+            </p>
+            <p className="text-neutral-500">
+              Followers
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
