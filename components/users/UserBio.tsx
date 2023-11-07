b3bf721429f037ef5966d733328ac45af82bee92
Kade <kadekorn@gmail.com>
2023-11-07 06:18:09 -0700

    not a lot of new code or even new concept, but its so cool seeing how a site like twitter is built

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -19,18 +19,25 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
     if (!fetchedUser?.createdAt) {
       return null;
     }
-    return format(new Date(fetchedUser.createdAt), 'MMMM yyyy');
+    return format(new Date(fetchedUser.createdAt), "MMMM yyyy");
   }, [fetchedUser?.createdAt]);
 
   return (
-    <div className="border-b-[8px] border-red-800 pb-4">
-      {/* <div className="flex justify-end p-2">
+    <div className="border-b-[1px] border-neutral-800 pb-4">
+      <div className="flex justify-end p-2">
         {currentUser?.id == userId ? (
           <Button secondary label="Edit" onClick={() => {}} />
         ) : (
           <Button onClick={() => {}} label="Follow" secondary />
         )}
-      </div> */}
+      </div>
+      <div className="mt-8 px-4">
+        <div className="flex flex-col">
+          <p className="text-white text-2xl font-semibold">
+            {fetchedUser?.name}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
