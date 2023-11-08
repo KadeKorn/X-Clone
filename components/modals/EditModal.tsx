9ad172f6952dcbb873eed7fca63663fc25f46e69
Kade <kadekorn@gmail.com>
2023-11-08 08:15:12 -0700

    or useEffect its better to have individual fields instead of using an object as the object will return more errors

--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -1,5 +1,27 @@
+import useCurrentUser from "@/hooks/useCurrentUser"
+import useEditModal from "@/hooks/useEditModal";
+import useUser from "@/hooks/useUser";
+import { useEffect, useState } from "react";
+
 const EditModal = () => {
-  return (
+    const {data: currentUser } = useCurrentUser();
+    const {mutate: mutateFetchedUser } = useUser(currentUser?.id);
+    const editModal = useEditModal();
+
+    const [profileImage,setProfileImage] = useState();
+    const [coverImage,setCoverImage] = useState();
+    const [name, setnName] = useState();
+    const [username,setUserName] = useState();
+    const [bio,setBio] = useState();
+
+    useEffect(() => {
+        setProfileImage(currentUser?.profileImage);
+        setCoverImage(currentUser?.coverImage);
+        setnName(currentUser?.name);
+        setUserName(currentUser?.username);
+        setBio(currentUser?.bio)
+    }, [currentUser])
+    return (
     <div></div>
   )
 }
